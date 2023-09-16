63580be0b13f939b2c672472ed2c3f8ff9260b9d
Anoushka Dhar <128288400+anoushkadhar123@users.noreply.github.com>
2023-09-16 15:41:32 +0530

    index.js

--- index.js
+++ index.js
@@ -3,12 +3,19 @@ const main = document.getElementById("main");
 const form = document.getElementById("form");
 const search = document.getElementById("search");
 
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const user = search.value;
+  if (user) {
+    getUser(user);
+    search.value = "";
+  }
+});
 async function getUser(username) {
   const resp = await fetch(APIURL + username);
   const respData = await resp.json();
   createUserCard(respData);
   getRepos(username);
-  saveSearchHistory(username);
 }
 async function getRepos(username) {
   const resp = await fetch(APIURL + username + "/repos");
@@ -16,7 +23,23 @@ async function getRepos(username) {
   addReposToCard(respData);
 }
 function createUserCard(user) {
-  const cardHTML = `    <div class="card">      <div>        <img class="avatar" src="${user.avatar_url}" alt="${user.name}" />      </div>      <div class="user-info">        <h2>${user.name}</h2>        <p>${user.bio}</p>        <ul class="info">          <li>${user.followers}<strong>Followers</strong></li>          <li>${user.following}<strong>Following</strong></li>          <li>${user.public_repos}<strong>Public Repositories</strong></li>        </ul>        <div id="repos"></div>      </div>    </div>  `;
+  const cardHTML = `        
+  <div class="card">            
+    <div>                
+      <img class="avatar" src="${user.avatar_url}" alt="${user.name}" />            
+    </div>            
+    <div class="user-info">                
+      <h2>${user.name}</h2>                
+        <p>${user.bio}</p>                
+          <ul class="info">                    
+            <li>${user.followers}<strong>Followers</strong></li>                    
+            <li>${user.following}<strong>Following</strong></li>                    
+            <li>${user.public_repos}<strong>Public Repositories</strong></li>                
+          </ul>               
+            <div id="repos"></div>            
+    </div>      
+  </div>    
+  `;
   main.innerHTML = cardHTML;
 }
 function addReposToCard(repos) {
@@ -33,80 +56,3 @@ function addReposToCard(repos) {
       reposEl.appendChild(repoEl);
     });
 }
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  const user = search.value;
-  if (user) {
-    getUser(user);
-    search.value = "";
-  }
-});
-getUser("anoushkadhar123");
-
-
-// Initialize search history array
-let searchHistory = [];
-form.addEventListener("submit",(e) => {
-  e.preventDefault();
-  const user = search.value;
-  if(user) {
-    getUser(user);
-    search.value = "";
-    searchHistory.push(user);
-    displaySearchHistory();
-  }
-});
-function displaySearchHistory() {
-  const historyContainer = document.getElementById("search-history"); 
-  historyContainer.innerHTML = "";    
-  searchHistory.forEach((term) => {        
-    const termEl = document.createElement("div");        
-    termEl.innerText = term;        
-    historyContainer.appendChild(termEl);    
-  });
-}
-function clearSearchHistory() {
-  searchHistory = [];
-  displaySearchHistory();
-}
-
-// Function to add search item to history
-function addToSearchHistory(username) {
-  // Check if username already exists in history    
-  const existingItemIndex = searchHistory.findIndex(item => item.username === username);
-  // If username exists, update the search count    
-  if (existingItemIndex !== -1) {
-    searchHistory[existingItemIndex].count++;
-  }
-  else {
-    // If username does not exist, add it to the history        
-    searchHistory.push({ username: username, count: 1 });
-  }
-}
-// Function to sort the history by oldest
-function sortHistoryByOldest() {    
-  searchHistory.sort((a, b) => {        
-    return new Date(a.date) - new Date(b.date);    
-  });
-}
-// Function to sort the history by newest
-function sortHistoryByNewest() {    
-  searchHistory.sort((a, b) => {        
-    return new Date(b.date) - new Date(a.date);    
-  });
-}
-// Function to sort the history by most searched
-function sortHistoryByMostSearched() {    
-  searchHistory.sort((a, b) => {        
-    return b.count - a.count;    
-  });
-}
-// Sort the history by oldest
-sortHistoryByOldest();
-console.log(searchHistory);
-// Sort the history by newest
-sortHistoryByNewest();
-console.log(searchHistory);
-// Sort the history by most searched
-sortHistoryByMostSearched();
-console.log(searchHistory);
