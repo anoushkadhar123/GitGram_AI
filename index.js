888090da0afa724d43aa173dc9a2b7e95098fc5d
Anoushka Dhar <128288400+anoushkadhar123@users.noreply.github.com>
2023-09-16 14:44:47 +0530

    index.js

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ async function getUser(username) {
   const respData = await resp.json();
   createUserCard(respData);
   getRepos(username);
+  saveSearchHistory(username);
 }
 async function getRepos(username) {
   const resp = await fetch(APIURL + username + "/repos");
@@ -41,3 +42,71 @@ form.addEventListener("submit", (e) => {
   }
 });
 getUser("anoushkadhar123");
+
+
+// Initialize search history array
+let searchHistory = [];
+form.addEventListener("submit",(e) => {
+  e.preventDefault();
+  const user = search.value;
+  if(user) {
+    getUser(user);
+    search.value = "";
+    searchHistory.push(user);
+    displaySearchHistory();
+  }
+});
+function displaySearchHistory() {
+  const historyContainer = document.getElementById("search-history"); 
+  historyContainer.innerHTML = "";    
+  searchHistory.forEach((term) => {        
+    const termEl = document.createElement("div");        
+    termEl.innerText = term;        
+    historyContainer.appendChild(termEl);    
+  });
+}
+function clearSearchHistory() {
+  searchHistory = [];
+  displaySearchHistory();
+}
+
+// Function to add search item to history
+function addToSearchHistory(username) {
+  // Check if username already exists in history    
+  const existingItemIndex = searchHistory.findIndex(item => item.username === username);
+  // If username exists, update the search count    
+  if (existingItemIndex !== -1) {
+    searchHistory[existingItemIndex].count++;
+  }
+  else {
+    // If username does not exist, add it to the history        
+    searchHistory.push({ username: username, count: 1 });
+  }
+}
+// Function to sort the history by oldest
+function sortHistoryByOldest() {    
+  searchHistory.sort((a, b) => {        
+    return new Date(a.date) - new Date(b.date);    
+  });
+}
+// Function to sort the history by newest
+function sortHistoryByNewest() {    
+  searchHistory.sort((a, b) => {        
+    return new Date(b.date) - new Date(a.date);    
+  });
+}
+// Function to sort the history by most searched
+function sortHistoryByMostSearched() {    
+  searchHistory.sort((a, b) => {        
+    return b.count - a.count;    
+  });
+}
+// Sort the history by oldest
+sortHistoryByOldest();
+console.log(searchHistory);
+// Sort the history by newest
+sortHistoryByNewest();
+console.log(searchHistory);
+// Sort the history by most searched
+sortHistoryByMostSearched();
+console.log(searchHistory);
